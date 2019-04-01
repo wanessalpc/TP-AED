e92886bbef9e511c161284cd6090c74b651a290e
MarcusQ <bhzmarcus@gmail.com>
2019-03-31 21:13:36 -0300

    Arquivos nas classes

--- Curriculos/Curriculos/FilaVagas.cs
+++ Curriculos/Curriculos/FilaVagas.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -57,6 +58,11 @@ namespace Curriculos
             Vagas NovaVaga = new Vagas(Validade, Area, Escolaridade, Salario, NomeEmpresa);
             vagaUlt.VagaProximo = NovaVaga;
             vagaUlt = NovaVaga;
+
+            // Adiciona no arquivo uma linha com os dados da nova vaga.
+            string arquivo = Area + ".txt";
+            using (StreamWriter writer = new StreamWriter(arquivo, true))
+                writer.WriteLine(Area + "-" + Escolaridade + "-" + Salario + "-" + NomeEmpresa + "-" + Validade);
         }
         public void Remover() // Remove a primeira vaga da fila.
         {
@@ -64,6 +70,12 @@ namespace Curriculos
             vagaPrim.VagaProximo = vagaPrim.VagaProximo.VagaProximo; // Aponta para o proximo da fila.
             if (vagaPrim.VagaProximo == null) // Se fila Vazia => alterar vagaUlt para lista vazia.
                 vagaUlt = vagaPrim;
+
+            // Deleta primeira linha do arquivo de texto.
+            string arquivo = vagaPrim.Area + ".txt";
+            var lines = File.ReadAllLines(arquivo).Skip(1);
+            File.Delete(arquivo);
+            File.WriteAllLines(arquivo, lines);
         }
         public List<Vagas> Buscar(string NomeEmpresa) // Retorna lista de vagas da empresa informada.
         {
@@ -88,6 +100,19 @@ namespace Curriculos
                 Aux = Aux.VagaProximo;
             }
             return print.ToString();
+        }
+        public void LerArquivo() // Lê arquivo e cria vagas se nescessario.
+        {
+            string arquivo = vagaPrim.Area + ".txt";
+            using (StreamReader reader = new StreamReader(arquivo))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string linha = reader.ReadLine();
+                    string[] dados = linha.Split('-');
+                    Adicionar(Convert.ToDateTime(dados[0]), VagaPrimeiro.Area, dados[1], Convert.ToDouble(dados[2]), dados[3]);
+                }
+            }
         }
         public bool Vazia() // returna true se fila esta vazia.
         {
--- Curriculos/Curriculos/GerenciadorVagas.cs
+++ Curriculos/Curriculos/GerenciadorVagas.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,26 +26,79 @@ namespace Curriculos
         }
 
         //Métodos
-        public void AdicionarVaga()
+        public void AdicionarVaga(DateTime Validade, string Area, string Escolaridade, double Salario, string NomeEmpresa)
         {
-
+            FilaVagas Aux = FilaPrim.FilaVagaProximo;
+            while (Aux != null) // Percorre toda a lista de FilaVagas.
+            {
+                if (Aux.VagaPrimeiro.Area == Area) // Caso encontre Area compativel => Adiciona vaga na fila da area.
+                {
+                    Aux.Adicionar(Validade, Area, Escolaridade, Salario, NomeEmpresa);
+                    return;
+                }
+                Aux = Aux.FilaVagaProximo;
+            }
+            FilaVagas NovaFila = new FilaVagas(Area); // Caso não encontre Area compativel => instancia nova Fila para Area.
+            FilaUlt.FilaVagaProximo = NovaFila;
+            FilaUlt = NovaFila;
+            FilaUlt.Adicionar(Validade, Area, Escolaridade, Salario, NomeEmpresa); // Adiciona a Vaga na nova fila instanciada.
+            // Salva em texto arquivos de areas criados.
+            using (StreamWriter writer = new StreamWriter("ListaArquivos.txt", true))
+            {
+                string arquivo = Area;
+                writer.WriteLine(arquivo);
+            }
         }
-        
-        public void RemoverVaga()
+        public void RemoverVaga(string Area) // Remove vaga da fila da Area informada.
         {
-
+            FilaVagas Aux = FilaPrim.FilaVagaProximo;
+            while (Aux != null)  // Percorre toda a lista de FilaVagas.
+            {
+                if (Aux.VagaPrimeiro.Area == Area) // Caso encontre Area compativel => Remove primeira vaga na fila da area.
+                {
+                    Aux.Remover();
+                    return;
+                }
+                Aux = Aux.FilaVagaProximo;
+            }
+            throw new Exception("Não ha vagas disponíveis nesta área."); // Caso não encontre Area compativel => throw Exception.
         }
-
-        public void BuscarFila()
+        public List<Vagas> BuscarFila(string Area, string NomeEmpresa)
         {
-
+            FilaVagas Aux = FilaPrim.FilaVagaProximo;
+            List<Vagas> result = new List<Vagas>();
+            while (Aux != null)
+            {
+                if (Aux.VagaPrimeiro.Area == Area)
+                    result = Aux.Buscar(NomeEmpresa);
+                Aux = Aux.FilaVagaProximo;
+            }
+            return result;
         }
 
+        private void LerArquivos() // Metodo só é chamado no Construtor, para ler arquivos e instanciar objetos.
+        {
+            if (File.Exists("ListaArquivos.txt"))
+            {
+                using (StreamReader reader = new StreamReader("ListaArquivos.txt"))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        string linha = reader.ReadLine();
+                        FilaVagas NovaFila = new FilaVagas(linha);
+                        FilaUlt.FilaVagaProximo = NovaFila;
+                        FilaUlt = NovaFila;
+                        FilaUlt.LerArquivo();
+                    }
+                }
+            }
+        }
         //Construtor
         public GerenciadorVagas()
         {
             FilaPrim = new FilaVagas(null);
             FilaUlt = FilaPrim;
+            LerArquivos();
         }
     }
 }
